1d91e349d8e4d8d3ace1b613b60b8f312de6fd7b
Lolz247 <103297129+Lolz247@users.noreply.github.com>
2023-03-04 00:39:03 -0500

    periodic table colors

--- Const.java
+++ Const.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class Const {
     public static final int X_SPACE = 50;
     public static final int Y_SPACE = 80;
@@ -5,5 +7,13 @@ public class Const {
     public static final int ELEMENT_SPACE = 80;
     public static final int ELEMENT_SIZE = 60;
 
+    public static final Color PURPLE = new Color(140, 82, 255);
+    public static final Color PINK = new Color(255, 112, 253);
+    public static final Color ORANGE = new Color(255, 145, 77);
+    public static final Color DANIEL = new Color(255, 189, 89); 
+    public static final Color LIME = new Color(193, 255, 114);
+    public static final Color CYAN = new Color(12, 192, 223);
+    public static final Color BLUE = new Color(82, 113, 255);
+    public static final Color BROWN = new Color(178, 140, 90);
     private Const(){}
 }
--- PeriodicTable.java
+++ PeriodicTable.java
@@ -10,7 +10,16 @@ public class PeriodicTable {
   final int elementSpace = Const.ELEMENT_SPACE;
   final int XSpace = Const.X_SPACE;
   final int YSpace = Const.Y_SPACE;
-  Color PURPLE = new Color(159, 43, 104);
+
+  Color purple = Const.PURPLE;
+  Color pink = Const.PINK; 
+  Color orange = Const.ORANGE;
+  Color daniel = Const.DANIEL;
+  Color lime = Const.LIME;
+  Color cyan = Const.CYAN;
+  Color blue = Const.BLUE;
+  Color brown = Const.BROWN;
+
   ArrayList<JButton> buttons = new ArrayList<>();
   
 //------------------------------------------------------------------------------
@@ -28,42 +37,30 @@ public class PeriodicTable {
     for (int i=0;i<elements.length;i++){ //what is this
       int group = elements[i].getGroup();
       int period = elements[i].getPeriod();
+      g.setColor(Color.RED);
       switch(elements[i].getType()){
-        case "Akali Metal":
+        case "Akali Metal": g.setColor(orange); break;
+        case "Alkaline Earth Metal": g.setColor(daniel); break;
+        case "Noble Gas": g.setColor(purple); break;
+        case "Metal": g.setColor(blue); break;
+        case "Transition Metal": g.setColor(Color.RED); break;
+        case "Metalloid": g.setColor(cyan); break;
+        case "Nonmetal": g.setColor(lime); break;
+        case "Halogen": g.setColor(Color.GREEN); break;
+        case "Lanthanide": g.setColor(brown); break;
+        case "Actinide": g.setColor(purple); break;
+        case "Transactinide": g.setColor(Color.GRAY); break;
+      }
+      if(i > 55 && i < 71){
+        period+=3;
+        group = i-55+2;
+      }
+      if(i > 87 && i < 103){
+        period+=3;
+        group = i-87+2;
       }
       g.fillOval(XSpace + (group-1)*elementSpace, YSpace + (period-1)*elementSpace, circleSize, circleSize);
     }
-        //   for (int g1 = 0; g1 < 6; g1++){//i hate daniel
-        //     g.fillOval(50, SPACING * g1 + 30 + SPACING, CIRCLESIZE, CIRCLESIZE);
-        //   }
-        //   g.setColor(Color.PINK); //Alkaline Earth Metals
-        //   for (int g2 = 0; g2 < 6; g2++){
-        //     g.fillOval(50 + SPACING, SPACING * g2 + 30 + SPACING, CIRCLESIZE, CIRCLESIZE);
-        //   }
-        //   g.setColor(Color.RED); //Transition Metals
-        //   for (int g3 = 0; g3 < 2; g3++){
-        //     g.fillOval(50 + 2*SPACING, SPACING * g3 + 30 + 3*SPACING, CIRCLESIZE, CIRCLESIZE);
-        //   }
-        //   for (int g4 = 3; g4 < 8; g4++){
-        //     for (int j4 = 2; j4 < 6; j4++){
-        //         g.fillOval(50 + g4*SPACING, SPACING + 30 + j4*SPACING, CIRCLESIZE, CIRCLESIZE);
-        //     }
-        //   }
-        //   for (int g5 = 8; g5 < 12; g5++){
-        //     for (int j5 = 2; j5 < 5; j5++){
-        //         g.fillOval(50 + g5*SPACING, SPACING + 30 + j5*SPACING, CIRCLESIZE, CIRCLESIZE);
-        //     }
-        //   }
-
-        //   g.setColor(Color.GRAY); //Lanthanoids
-        //   for (int p1 = 0; p1 < 15; p1++){
-        //     g.fillOval(SPACING * p1 + 50 + 3*SPACING, 30 + 8*SPACING, CIRCLESIZE, CIRCLESIZE);
-        //   }
-        //   g.setColor(Color.PINK); //Actinoids
-        //   for (int p1 = 0; p1 < 15; p1++){
-        //     g.fillOval(SPACING * p1 + 50 + 3*SPACING, 30 + 9*SPACING, CIRCLESIZE, CIRCLESIZE);
-        //   }
-        // }
   }
 //------------------------------------------------------------------------------
 }
