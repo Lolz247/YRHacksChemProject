0d79431aee6741bb1b6c802ac28d181056e7c565
Dandan937 <35547415+Dandan937@users.noreply.github.com>
2023-03-03 19:39:43 -0500

    Update Main.java
    jframe

--- Main.java
+++ Main.java
@@ -1,6 +1,92 @@
-public class Main{
+// main java file that runs jpanel and jframe
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+  JFrame gameWindow;
+  GamePanel gamePanel;   
+  MyKeyListener keyListener; 
+  MyMouseListener mouseListener;
+  
+  boolean escpressed;
+  
+//------------------------------------------------------------------------------
+  Main(){
+    gameWindow = new JFrame("Pocket Chem (Lite)");
+    gameWindow.setSize(1600,900);
+    gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    gameWindow.setResizable(false);
+    
+    gamePanel = new GamePanel();
+    gameWindow.add(gamePanel); 
+    
+    keyListener = new MyKeyListener();
+    gamePanel.addKeyListener(keyListener);
+    
+    mouseListener = new MyMouseListener();
+    gamePanel.addMouseListener(mouseListener);
+    
+    gameWindow.setVisible(true);    
+  }
+//------------------------------------------------------------------------------  
+  //main game loop
+  public void run(){
+    // code (while loop)
+    while (true) {
+        gameWindow.repaint();
+        try {Thread.sleep(16);} catch(Exception e){}
+    }
+  }
+//------------------------------------------------------------------------------  
+    //act upon key events
+    public class MyKeyListener implements KeyListener{
+        public void keyPressed(KeyEvent e){
+            //int key = e.getKeyCode();
+        }
+        public void keyReleased(KeyEvent e){ 
+            //int key = e.getKeyCode();
+        }   
+        public void keyTyped(KeyEvent e){
+        }           
+    }
+//------------------------------------------------------------------------------
+    // mouse inputs
+    public class MyMouseListener implements MouseListener{
+        public void mouseClicked(MouseEvent e){   // gets the mouse coordinates
+            int mouseX = e.getX();
+            int mouseY = e.getY();
+            System.out.println(mouseX + " " + mouseY);
+        }
+        public void mousePressed(MouseEvent e){   // MUST be implemented even if not used!
+        }
+        public void mouseReleased(MouseEvent e){  // MUST be implemented even if not used!
+        }
+        public void mouseEntered(MouseEvent e){   // MUST be implemented even if not used!
+        }
+        public void mouseExited(MouseEvent e){    // MUST be implemented even if not used!
+        }
+    }
+//------------------------------------------------------------------------------
+    //draw everything
+    public class GamePanel extends JPanel{
+        GamePanel(){
+            setFocusable(true);
+            requestFocusInWindow();
+        }
+        
+        @Override
+        public void paintComponent(Graphics g){ 
+          super.paintComponent(g); //required
+          g.setColor(Color.GRAY);
+          g.fillRect(300,300,1000,5);
+        }
+    }    
+//------------------------------------------------------------------------------
     public static void main(String[] args){
         System.out.println("we love chemistry!!!"); //i hate daniel 
-        // hey who said that wtf
+        Main thing = new Main();
+        thing.run();
     }
 }
