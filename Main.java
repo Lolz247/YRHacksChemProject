cb0eb7ab119135fc76d75b31cab5fa0e4cc02d30
soup0123 <06kevinhua@gmail.com>
2023-03-03 19:20:49 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("we love chemistry!!!");
+        System.out.println("we love chemistry!!!"); //i hate daniel
     }
 }
