0db4014932ec7508a79b0a7ce5ad3a827fbbc69a
Dandan937 <35547415+Dandan937@users.noreply.github.com>
2023-03-03 19:22:19 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("we love chemistry!!!"); //i hate daniel
+        System.out.println("we love chemistry!!!"); //i hate daniel 
+        // hey who said that wtf
     }
 }
